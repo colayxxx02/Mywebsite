6d00ce44e9229097a81f3ea3d8ff7f52f1dd01ef
colayxxx02 <polinar.nica2004@gmail.com>
2026-08-17 18:39:48 +0800

    Delete sis directory

--- sis/admin/process_delete_subject.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../config/db.php');
-if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-    mysqli_query($conn, "DELETE FROM subjects WHERE id = $id");
-    header("Location: course_sched.php?deleted");
-}
-?>
